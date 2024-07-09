079df1693b95ce83eb0c7ae580576454023ea7ef
VectorYQ <168515540+VectorYQ@users.noreply.github.com>
2024-07-09 15:11:39 +0800

    Add files via upload
    add my learning files

---
+++ js/index.js
@@ -0,0 +1,81 @@
+const textarea = document.getElementById("code");
+const reset = document.getElementById("reset");
+const solution = document.getElementById("solution");
+const output = document.querySelector(".output");
+const code = textarea.value;
+let userEntry = textarea.value;
+
+function updateCode() {
+  output.innerHTML = textarea.value;
+}
+
+const htmlSolution = `<h1>静夜思</h1>
+<p>床前明月光 疑是地上霜</p>
+<p>举头望明月 低头思故乡</p>`;
+
+let solutionEntry = htmlSolution;
+
+reset.addEventListener("click", () => {
+  textarea.value = code;
+  userEntry = textarea.value;
+  solutionEntry = htmlSolution;
+  solution.value = "显示答案";
+  updateCode();
+});
+
+solution.addEventListener("click", () => {
+  if (solution.value === "显示答案") {
+    textarea.value = solutionEntry;
+    solution.value = "隐藏答案";
+  } else {
+    textarea.value = userEntry;
+    solution.value = "显示答案";
+  }
+  updateCode();
+});
+
+textarea.addEventListener("input", updateCode);
+window.addEventListener("load", updateCode);
+
+// Stop tab key tabbing out of textarea and
+// make it write a tab at the caret position instead
+textarea.onkeydown = (e) => {
+  if (e.keyCode === 9) {
+    e.preventDefault();
+    insertAtCaret("\t");
+  }
+
+  if (e.keyCode === 27) {
+    textarea.blur();
+  }
+};
+
+function insertAtCaret(text) {
+  const scrollPos = textarea.scrollTop;
+  let caretPos = textarea.selectionStart;
+
+  const front = textarea.value.substring(0, caretPos);
+  const back = textarea.value.substring(
+    textarea.selectionEnd,
+    textarea.value.length,
+  );
+  textarea.value = front + text + back;
+  caretPos += text.length;
+  textarea.selectionStart = caretPos;
+  textarea.selectionEnd = caretPos;
+  textarea.focus();
+  textarea.scrollTop = scrollPos;
+}
+
+// Update the saved userCode every time the user updates the text area code
+textarea.onkeyup = function () {
+  // We only want to save the state when the user code is being shown,
+  // not the solution, so that solution is not saved over the user code
+  if (solution.value === "显示答案") {
+    userEntry = textarea.value;
+  } else {
+    solutionEntry = textarea.value;
+  }
+
+  updateCode();
+};
